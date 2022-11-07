7aafa51f16ae9bc6c0fc979b094c01b90945d74e
doorigin <wm05007@gmail.com>
2022-11-07 23:30:50 +0900

    added basic oauth

--- backend/domain/post/post_router.py
+++ backend/domain/post/post_router.py
@@ -1,23 +1,146 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status, FastAPI
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from datetime import datetime
+from datetime import datetime, timedelta
 from database import get_db
 from domain.post import post_schema
-from models import Post, Comment
+from models import Post, Comment, User
 from pydantic import BaseModel
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
 router = APIRouter(
-    prefix="/api/post",
-    tags=["post"]
+    prefix="/api"
+    # tags=["post"]
 )
 
-@router.get("/list", response_model=list[post_schema.Question])
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/token")
+
+app = FastAPI()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(db, username: str):
+    user = db.query(User).filter(User.username == username).first()
+    if user:
+        return post_schema.UserInDB(
+            username = user.username,
+            email = user.email,
+            full_name = user.full_name,
+            disabled = user.disabled,
+            hashed_password = user.hashed_password
+        )
+
+def authenticate_user(db: Session, username: str, password: str):
+    u = get_user(db, username)
+    if not u:
+        return False
+    if not verify_password(password, u.hashed_password):
+        return False
+    return u
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+@router.post("/token", response_model=Token, tags=["user"])
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.post("/users/create", tags=["user"])
+async def user_create(_user_create: post_schema.UserCreate, db: Session = Depends(get_db)):
+    db_user = User(username=_user_create.username,
+                full_name=_user_create.full_name,
+                hashed_password=pwd_context.hash(_user_create.password1),
+                email=_user_create.email, 
+                disabled = False)
+    db.add(db_user)
+    db.commit()
+    return {"Status":"ok", "data": "user created"}
+
+@router.get("/users/me/", response_model=post_schema.User, tags=["user"])
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+@router.get("/users/me/items/", tags=["user"])
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
+
+@router.get("/list", response_model=list[post_schema.Post])
 def question_list(db: Session = Depends(get_db)):
     _question_list = db.query(Post).order_by(Post.create_date.desc()).all()
     return _question_list
 
-@router.get("/search/{search_tag}", response_model=list[post_schema.Question])
-def question_list(search_tag: str, db: Session = Depends(get_db)):
+@router.get("/search/{search_tag}", response_model=list[post_schema.Post])
+def question_search(search_tag: str, db: Session = Depends(get_db)):
     _question_list = db.query(Post).filter(Post.subject.contains(search_tag)).order_by(Post.create_date.desc()).all()
     return _question_list
 
@@ -33,13 +156,13 @@ def create_item(post: GetPost, db: Session = Depends(get_db)):
     db.commit()
     return {"Status":"ok", "data": "item commited to DB"}
 
-@router.get("/{id}/detail", response_model=post_schema.Question)
-def question_list(id: int, db: Session = Depends(get_db)):
+@router.get("/{id}/detail", response_model=post_schema.Post)
+def question_detail(id: int, db: Session = Depends(get_db)):
     _question = db.query(Post).get(id)
     return _question
 
-@router.put("/{id}/update", response_model=post_schema.Question)
-def question_list(id: int, post: GetPost, db: Session = Depends(get_db)):
+@router.put("/{id}/update", response_model=post_schema.Post)
+def question_update(id: int, post: GetPost, db: Session = Depends(get_db)):
     q = db.query(Post).get(id)
     q.subject = post.subject
     q.content = post.content
--- backend/domain/post/post_schema.py
+++ backend/domain/post/post_schema.py
@@ -1,9 +1,6 @@
 import datetime
-
-from pydantic import BaseModel
-
-
-class Question(BaseModel):
+from pydantic import BaseModel, validator, EmailStr
+class Post(BaseModel):
     id: int
     subject: str
     content: str
@@ -17,6 +14,37 @@ class Comment(BaseModel):
     content: str
     question: int
     create_date: datetime.datetime
+
+    class Config:
+        orm_mode = True
+
+class UserCreate(BaseModel):
+    username: str
+    full_name: str
+    password1: str
+    password2: str
+    email: EmailStr
+
+    @validator('username', 'password1', 'password2', 'email')
+    def not_empty(cls, v):
+        if not v or not v.strip():
+            raise ValueError('빈 값은 허용되지 않습니다.')
+        return v
+
+    @validator('password2')
+    def passwords_match(cls, v, values):
+        if 'password1' in values and v != values['password1']:
+            raise ValueError('비밀번호가 일치하지 않습니다')
+        return v
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
 
     class Config:
         orm_mode = True
